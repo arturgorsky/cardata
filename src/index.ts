482344a59a3ded82b63c08ffd74a9af6c32cad82
arturgorsky <gorski.artur1@gmail.com>
2022-06-12 14:35:49 +0200

    add-axios-and-get-total-number-of-vechicles

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello");
+import axios from "axios";
+
+const url =
+  "https://www.otomoto.pl/i2/ajax/ad-item/total/?search[category_id]=29";
+
+const getNumberOfCars = async () => {
+  const numberOfCars = await (await axios.get(url)).data.total_count;
+  const timestamp = new Date();
+  console.log(timestamp, numberOfCars);
+};
+
+getNumberOfCars();
